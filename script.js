057ee7fba68e58cadd4e1b26c2bd8864b15e1e69
Nicholas-Battista <battistan23@gmail.com>
2023-10-05 17:44:41 -0700

    created a game function in js
    created a game function that runs playround as long as user and computer
    do not have 5 wins. displays score each round. stops once 5 is reached

--- script.js
+++ script.js
@@ -4,52 +4,81 @@ function getComputerChoice() {
     return choice;
 }
 
+let userscore = 0;
+let computerscore = 0;
 
 function playRound(playerChoice, computerChoice) {
 
     while (playerChoice === "rock") {
         if (computerChoice === "scissors") {
+            userscore++;
             return "You win! rock beats scissors!";
         }
         else if (computerChoice === "rock") {
             return "You Tied! rock ties rock!";
         }
         else {
+            computerscore++;
             return "You Lose! paper beats rock!";
         }    
     }
 
     while (playerChoice === "paper") {
         if (computerChoice === "rock") {
+            userscore++;
             return "You win! paper beats rock!";
         }
         else if (computerChoice === "paper") {
             return "You Tied! paper ties paper!";
         }
         else {
+            computerscore++;
             return "You Lose! scissors beats paper!";
         }    
     }
 
     while (playerChoice === "scissors") {
         if (computerChoice === "paper") {
+            userscore++;
             return "You win! scissors beats paper!";
         }
         else if (computerChoice === "scissors") {
             return "You Tied! scissors ties scissors!";
         }
         else {
+            computerscore++;
             return "You Lose! rock beats scissors!";
         }    
     }
     }
 
-const computerChoice = getComputerChoice();
-const playerChoice = prompt("You are battling a AI ROBOT! Quick choose between rock paper or scissors!!");
-console.log(computerChoice);
-console.log(playerChoice);
+function game() {
+    //for (i = 0; i <= 5; i++) {
+        //playerChoice = prompt("Whats your next choice?");
+        //computerChoice = getComputerChoice();
+        //console.log(computerChoice);
+        //console.log(playerChoice);
 
-alert(playRound(playerChoice, computerChoice));
+        //alert(playRound(playerChoice, computerChoice));
+        //alert("Your score: " + userscore + "            Computer score: " + computerscore);
+    
+
+    while (userscore !== 5 && computerscore !== 5) {
+        playerChoice = prompt("Whats your next choice?");
+        computerChoice = getComputerChoice();
+        console.log(computerChoice);
+        console.log(playerChoice);
+
+        alert(playRound(playerChoice, computerChoice));
+        alert("Your score: " + userscore + "            Computer score: " + computerscore);
+    }
+}
+
+let computerChoice;
+let playerChoice;
+alert("You are battling a AI ROBOT! Quick choose between rock paper or scissors!!");
+
+game();
 
 
 // if (playerChoice === "rock" && computerChoice === "scissors") {
