f17f0e9c72e9ecec39c8958ee930fe39e2ed1f82
Nicholas-Battista <battistan23@gmail.com>
2023-10-05 17:08:32 -0700

    created a function to play one round of rps
    playround function uses while loops with nested ifs with the computer
    choice and user choice to play one round of rps

--- script.js
+++ script.js
@@ -4,4 +4,60 @@ function getComputerChoice() {
     return choice;
 }
 
-console.log(getComputerChoice());
+
+function playRound(playerChoice, computerChoice) {
+
+    while (playerChoice === "rock") {
+        if (computerChoice === "scissors") {
+            return "You win! rock beats scissors!";
+        }
+        else if (computerChoice === "rock") {
+            return "You Tied! rock ties rock!";
+        }
+        else {
+            return "You Lose! paper beats rock!";
+        }    
+    }
+
+    while (playerChoice === "paper") {
+        if (computerChoice === "rock") {
+            return "You win! paper beats rock!";
+        }
+        else if (computerChoice === "paper") {
+            return "You Tied! paper ties paper!";
+        }
+        else {
+            return "You Lose! scissors beats paper!";
+        }    
+    }
+
+    while (playerChoice === "scissors") {
+        if (computerChoice === "paper") {
+            return "You win! scissors beats paper!";
+        }
+        else if (computerChoice === "scissors") {
+            return "You Tied! scissors ties scissors!";
+        }
+        else {
+            return "You Lose! rock beats scissors!";
+        }    
+    }
+    }
+
+const computerChoice = getComputerChoice();
+const playerChoice = prompt("You are battling a AI ROBOT! Quick choose between rock paper or scissors!!");
+console.log(computerChoice);
+console.log(playerChoice);
+
+alert(playRound(playerChoice, computerChoice));
+
+
+// if (playerChoice === "rock" && computerChoice === "scissors") {
+//    return "You win! rock beats scissors!";
+//}
+//else if (playerChoice === "rock" && computerChoice === "rock") {
+//    return "You Tied! rock ties rock!";
+//}
+//else {
+//    return "You Lose! paper beats rock!";
+//}
