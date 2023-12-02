03dc5fc5e7a96ab16ab49cd70ab8cee43895e0a3
Nicholas-Battista <battistan23@gmail.com>
2023-12-01 23:29:09 -0700

    Signficantly improved effeciency of playround func
    Updated the playround funtion to take less lines of code and not use
    while loops. Updated the function to include the use of the choice
    buttons. Added results and score displayed on page.

--- script.js
+++ script.js
@@ -6,71 +6,71 @@ function getComputerChoice() {
 
 let userscore = 0;
 let computerscore = 0;
+let playerChoice;
+let btns = document.querySelectorAll('button');
+let computerChoice;
 let displayUserScore = document.querySelector('.user');
 let displayCompScore = document.querySelector('.comp');
 let description = document.querySelector('.description')
 
- function playRound(playerChoice, computerChoice) {
-    if (playerChoice === "rock" || playerChoice === "paper" || playerChoice === "scissors") {
-        while (playerChoice === "rock") {
-            if (computerChoice === "scissors") {
-                userscore++;
-                return "Your choice: rock        Computer: scissors\n\nYou win! rock beats scissors!";
-            }
-            else if (computerChoice === "rock") {
-                return "Your choice: rock        Computer: rock\n\nYou Tied! rock ties rock!";
-            }
-            else {
-                computerscore++;
-                return "Your choice: rock        Computer: paper\n\nYou Lose! paper beats rock!";
-            }    
-        }
+let popUp = document.querySelector('choices');
+let content = document.querySelector('.results');
+let showScore = document.querySelector('.score');
+
+let outcome;
+
+
+    btns.forEach( btn => {
+        btn.addEventListener('click', () => {
+            playerChoice = btn.textContent;
+            computerChoice = getComputerChoice();
+            console.log(computerChoice);
+            console.log(playerChoice);
+
+            playRound();
+        });
+    });
+
     
-        while (playerChoice === "paper") {
-            if (computerChoice === "rock") {
-                userscore++;
-                return "Your choice: paper        Computer: rock\n\nYou win! paper beats rock!";
-            }
-            else if (computerChoice === "paper") {
-                return "Your choice: paper        Computer: paper\n\nYou Tied! paper ties paper!";
-            }
-            else {
-                computerscore++;
-                return "Your choice: paper        Computer: scissors\n\nYou Lose! scissors beats paper!";
-            }    
+
+    function playRound() {
+        if (playerChoice === "Rock" && computerChoice === "scissors") {
+            userscore++
+            outcome = "Win!"
         }
-    
-        while (playerChoice === "scissors") {
-            if (computerChoice === "paper") {
-                userscore++;
-                return "Your choice: scissors        Computer: paper\n\nYou win! scissors beats paper!";
-            }
-            else if (computerChoice === "scissors") {
-                return "Your choice: scissors        Computer: scissors\n\nYou Tied! scissors ties scissors!";
-            }
-            else {
-                computerscore++;
-                return "Your choice: scissors        Computer: rock\n\nYou Lose! rock beats scissors!";
-            }    
+        else if (playerChoice === "Rock" && computerChoice === "paper") {
+            computerscore++
+            outcome = "Lose!"
         }
-    }
-    else if (playerChoice === "STOP") {
-        alert("Game has been stopped. Goodbye!")
-    }
-    else {
-        return "This is not an option!"
+        else if (playerChoice === "Paper" && computerChoice === "rock") {
+            userscore++
+            outcome = "Win!"
+        }
+        else if (playerChoice === "Paper" && computerChoice === "scissors") {
+            computerscore++
+            outcome = "Lose!"
+        }
+        else if (playerChoice === "Scissors" && computerChoice === "paper") {
+            userscore++
+            outcome = "Win!"
+        }
+        else if (playerChoice === "Scissors" && computerChoice === "rock") {
+            computerscore++
+            outcome = "Lose!"
+        }
+        else outcome = "Tie!"
+
+        content.textContent = "You chose " + playerChoice + "! The computer chose " + computerChoice + "! you " + outcome;
+
+        showScore.textContent = "Your score: " + userscore + "\n Computer Score: " + computerscore;
     }
     
-    }
+
 
 function game() {
     while (userscore !== 5 && computerscore !== 5 && playerChoice !== "STOP") {
-        playerChoice = prompt("Choose between rock, paper, or scissors!");
-        computerChoice = getComputerChoice();
-        console.log(computerChoice);
-        console.log(playerChoice);
 
-        alert(playRound(playerChoice, computerChoice));
+        playRound();
         alert("Your score: " + userscore + "            Computer score: " + computerscore);
         displayUserScore.textContent = "Your score: " + userscore;
         displayCompScore.textContent = "Comp score: " + computerscore;
@@ -83,16 +83,7 @@ function game() {
         alert("The robots wit was too great!")
     }
 } 
-    let playerChoice;
-    let btns = document.querySelectorAll('button');
-    btns.forEach( btn => {
-        btn.addEventListener('click', () => {
-            playerChoice = btn.innerHTML;
-            console.log(playerChoice);
-        });
-    });
-
-let computerChoice;
+    
 
 
 
